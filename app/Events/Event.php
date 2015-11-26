62df2ad2e46220a77e5095e8115d89a3297ffd9e
Daniel Navarrete <daniel_navarrete@msn.com>
2015-11-26 13:13:26 -0500

    primer commit

---
+++ app/Events/Event.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Limakuchen\Events;
+
+abstract class Event
+{
+    //
+}
---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Limakuchen\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+
+class VerifyCsrfToken extends BaseVerifier
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        //
+    ];
+}
